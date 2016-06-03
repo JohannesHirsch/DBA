2fd82a0f750faef926f024c5e6b2b748fc5a0753
JohannesHirsch <johannes.hirsch2@students.fh-wels.at>
2016-06-03 09:51:18 +0200

    DataSet removed

--- FF_Bewerbe/FF_Bewerbe/Frm_Main.cs
+++ FF_Bewerbe/FF_Bewerbe/Frm_Main.cs
@@ -10,24 +10,20 @@ using System.Windows.Forms;
 
 namespace FF_Bewerbe
 {
-    public partial class Form1 : Form
+    public partial class Frm_Main : Form
     {
-        public Form1()
+        public Frm_Main()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // TODO: This line of code loads data into the 'dB_BewerbDataSet.tBewerbSet' table. You can move, or remove it, as needed.
-            this.tBewerbSetTableAdapter.Fill(this.dB_BewerbDataSet.tBewerbSet);
-
         }
 
         private void btnSpeichern_Click(object sender, EventArgs e)
         {
-            this.dgvBewerbe.EndEdit();
-            this.tBewerbSetTableAdapter.Update(this.dB_BewerbDataSet.tBewerbSet);
+
         }
     }
 }
---
+++ FF_Bewerbe/FF_Bewerbe/Frm_Mannschaften.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FF_Bewerbe
+{
+    public partial class Frm_Mannschaften : Form
+    {
+        public Frm_Mannschaften()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Mannschaften_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dB_BewerbDataSet1.tMannschaftSet' table. You can move, or remove it, as needed.
+            //this.tMannschaftSetTableAdapter.Fill(this.dB_BewerbDataSet.tMannschaftSet);
+
+        }
+
+        private void btnSpeichern_Click(object sender, EventArgs e)
+        {
+            //this.dgvMannschaften.EndEdit();
+            //this.tMannschaftSetTableAdapter.Update(this.dB_BewerbDataSet.tMannschaftSet);
+        }
+    }
+}
