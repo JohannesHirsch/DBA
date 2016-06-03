3af71b3a7d999d581ac1116b70a641bc46deaff6
JohannesHirsch <johannes.hirsch2@students.fh-wels.at>
2016-06-03 09:14:34 +0200

    Project added

---
+++ FF_Bewerbe/FF_Bewerbe/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FF_Bewerbe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dB_BewerbDataSet.tBewerbSet' table. You can move, or remove it, as needed.
+            this.tBewerbSetTableAdapter.Fill(this.dB_BewerbDataSet.tBewerbSet);
+
+        }
+
+        private void btnSpeichern_Click(object sender, EventArgs e)
+        {
+            this.dgvBewerbe.EndEdit();
+            this.tBewerbSetTableAdapter.Update(this.dB_BewerbDataSet.tBewerbSet);
+        }
+    }
+}
