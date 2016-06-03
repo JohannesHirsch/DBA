9531cd8df3e9d4b1fa35243b88f1cc7e9f40d1f0
JohannesHirsch <johannes.hirsch2@students.fh-wels.at>
2016-06-03 18:41:47 +0200

    Erste Lauffähige Version

---
+++ DBA_Bewerbe/DBA_Bewerbe/FrmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace DBA_Bewerbe
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgvBewerbe = new System.Windows.Forms.DataGridView();
+            this.btnSpeichern = new System.Windows.Forms.Button();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.datumDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tBewerbBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBewerbe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tBewerbBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvBewerbe
+            // 
+            this.dgvBewerbe.AutoGenerateColumns = false;
+            this.dgvBewerbe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBewerbe.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.datumDataGridViewTextBoxColumn});
+            this.dgvBewerbe.DataSource = this.tBewerbBindingSource;
+            this.dgvBewerbe.Location = new System.Drawing.Point(12, 12);
+            this.dgvBewerbe.Name = "dgvBewerbe";
+            this.dgvBewerbe.Size = new System.Drawing.Size(356, 142);
+            this.dgvBewerbe.TabIndex = 0;
+            // 
+            // btnSpeichern
+            // 
+            this.btnSpeichern.Location = new System.Drawing.Point(557, 349);
+            this.btnSpeichern.Name = "btnSpeichern";
+            this.btnSpeichern.Size = new System.Drawing.Size(75, 23);
+            this.btnSpeichern.TabIndex = 1;
+            this.btnSpeichern.Text = "Speichern";
+            this.btnSpeichern.UseVisualStyleBackColor = true;
+            this.btnSpeichern.Click += new System.EventHandler(this.btnSpeichern_Click);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // datumDataGridViewTextBoxColumn
+            // 
+            this.datumDataGridViewTextBoxColumn.DataPropertyName = "Datum";
+            this.datumDataGridViewTextBoxColumn.HeaderText = "Datum";
+            this.datumDataGridViewTextBoxColumn.Name = "datumDataGridViewTextBoxColumn";
+            // 
+            // tBewerbBindingSource
+            // 
+            this.tBewerbBindingSource.DataSource = typeof(DBA_Bewerbe.tBewerb);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 384);
+            this.Controls.Add(this.btnSpeichern);
+            this.Controls.Add(this.dgvBewerbe);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBewerbe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tBewerbBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvBewerbe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn datumDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource tBewerbBindingSource;
+        private System.Windows.Forms.Button btnSpeichern;
+    }
+}
+
